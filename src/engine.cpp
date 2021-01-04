882613dbedb2aa9750ea629657ed6b782131a91c
edmbernard <bernard.erwan@gmail.com>
2021-01-04 07:47:00 +0100

    feat(engine): add word reading from file

--- src/engine.cpp
+++ src/engine.cpp
@@ -4,9 +4,7 @@
 Engine::Engine(std::vector<std::string> &&wordList) : m_wordList(wordList) {
     newPuzzle();
     std::cout << m_wordList.size() << std::endl;
-    for (auto word : m_wordList) {
-        std::cout << word << std::endl;
-    }
+
 };
 
 void Engine::newPuzzle() {

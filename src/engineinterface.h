a80f15221c0d645c34df8b47bf2f61a7b2d7599c
edmbernard <bernard.erwan@gmail.com>
2021-02-04 22:13:18 +0100

    clean(interface): clean header, add comments

--- src/engineinterface.h
+++ src/engineinterface.h
@@ -23,25 +23,20 @@ public:
   Q_INVOKABLE GridModel *getGrid() { return &m_gridModel; }
   Q_INVOKABLE WildcardModel *getWord() { return &m_wildcardModel; }
   Q_INVOKABLE FoundWordModel *getFoundWords() { return &m_foundwordModel; }
+
   Q_INVOKABLE int getGridSize() { return m_gridSize; }
   Q_INVOKABLE uint64_t getFound() { return m_foundWords.size(); }
   Q_INVOKABLE int64_t getTotal() { return m_numberWords; }
 
+  //! Move internal index and update wildcard
   Q_INVOKABLE void previousWord();
+  //! Move internal index and update wildcard
   Q_INVOKABLE void nextWord();
 
   Q_INVOKABLE void addLetter(uint64_t index);
   Q_INVOKABLE void cleanLetter();
 
   Q_INVOKABLE void generateNewPuzzle();
-
-  void search();
-
-  Q_INVOKABLE void resetGridModel();
-
-  Q_INVOKABLE void resetWildcardModel();
-
-  Q_INVOKABLE bool fillWildcard(uint64_t index);
 
 signals:
   void updateMeta();
@@ -51,28 +46,43 @@ signals:
   void notify(QString message);
 
 private:
-  void incrScore(int value);
+  //! reset grid model with data from engine
+  void resetGridModel();
+
+  //! reset wildcard model with data from engine and ask to update meta
+  void resetWildcardModel();
+
+  //! Fill empty space of the wildcard with selected letter in the grid
+  bool fillWildcard(uint64_t index);
+
+  //! Search for the word filled in wildcard
+  void search();
 
   void incrIndex(int value);
-  void decrScoreReserve();
-  void resetScoreReserve();
 
   bool addFoundWord(const QString &word);
 
-  int m_gridSize;
-  int m_wordsPerPuzzle;
+  void incrScore(int value);
+  void decrScoreReserve();
+  void resetScoreReserve();
 
-  int64_t m_currentWordIndex = 0;
-  std::vector<std::string> m_foundWords;
-  int64_t m_numberWords = 0;
-  std::vector<uint64_t> m_pressedIndex;
+  int m_gridSize;       //!< Grid size
+  int m_wordsPerPuzzle; //!< Maximum Number of word per Puzzle
 
-  std::unique_ptr<vowels::Engine> m_engine;
-  int64_t m_playerScore = 0;
-  uint8_t m_scoreReserve;
-  GridModel m_gridModel;
-  WildcardModel m_wildcardModel;
-  FoundWordModel m_foundwordModel;
+  int64_t m_currentWordIndex = 0;       //!< Cursor of the current word in ui
+  int64_t m_numberWords = 0;            //!< Total number of words to find
+  std::vector<uint64_t> m_pressedIndex; //!< List of letter selected by player
+
+  int64_t m_playerScore = 0; //!< Total player score
+  uint8_t m_scoreReserve;    //!< Score reserve to add on next match
+
+  std::unique_ptr<vowels::Engine> m_engine; //!< Processing engine for all logic
+
+  GridModel m_gridModel;         //!< Data contained in the letter grid
+  WildcardModel m_wildcardModel; //!< Data contained in the woldcard world
+
+  FoundWordModel m_foundwordModel;       //!< List of found word for ui
+  std::vector<std::string> m_foundWords; //!< List of found word for processing
 };
 
 #endif // ENGINEINTERFACE_H
